9f9ea13c81c31eabe0801175421c120c6c2f003f
Jllanjech <juana.lc@hotmail.es>
2022-11-20 03:07:41 -0500

    Show the products connecting with the API

--- shopping-cart/src/components/Header/index.jsx
+++ shopping-cart/src/components/Header/index.jsx
@@ -1,38 +1,52 @@
-import './styles.scss'
+import "./styles.scss";
 
 export const Header = () => {
   return (
     <header className="header">
-        <h1>Shopping</h1>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="28"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+      >
+        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+      </svg>
+      <h1 className="header__h1">Shopping</h1>
       <button className="header__button">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-list"
           viewBox="0 0 16 16"
         >
           <path
-            fill-rule="evenodd"
             d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
           />
         </svg>
       </button>
       <nav className="header__nav">
-        
         <ul className="header__ul">
           <li className="header__li">
-            <a href="#" className="header__a">Categories</a>
+            <a href="#" className="header__a">
+              Categories
+            </a>
           </li>
           <li className="header__li">
-            <a href="#" className="header__a">About</a>
+            <a href="#" className="header__a">
+              About
+            </a>
           </li>
           <li className="header__li">
-            <a href="#" className="header__a">Blog</a>
+            <a href="#" className="header__a">
+              Blog
+            </a>
           </li>
           <li className="header__li">
-            <a href="#" className="header__a">Contact</a>
+            <a href="#" className="header__a">
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
--- shopping-cart/src/components/Products/index.jsx
+++ shopping-cart/src/components/Products/index.jsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from 'react'
 import { Item } from '../Item/index'
-
-export const Products = (props) => {
-    const { products } = props
+import { allProducts } from '../../../api/models/functions'
+export const Products = () => {
+    const [products, setProducts] =useState(null)
+    useEffect(() =>{
+    allProducts(setProducts)},[])
+  
     return (
         <main>
         <h1>Home</h1>
         <div className="row">
-            {products.map((product) => (
-            <Item key={product.id} item={product}/>))}
-        </div>
+            {products != null ? (products.map((product) => (
+            <div key={product._id}>
+            <img src={product.image}/>
+            <h1><a href="#">{product.name}</a></h1>
+            <h2>S/ {product.price}.00</h2>
+          </div>
+        ))) :('no hay productos')}</div>
         </main>
     )
 }
