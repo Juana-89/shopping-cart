92c41da7a8b94290208af2858fae741988fb07a8
Jllanjech <jllanje@bindit.com.pe>
2022-12-15 23:12:02 -0500

    Adding ico

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import { Announcements } from "./components/Announcements/index";
 import { Header } from "./components/Header/index";
 import { Hero } from "./components/Hero/index";
 import { Products } from "./components/Products/index";
 import { Publisher } from "./components/Publisher/index";
 import { Footer } from "./components/Footer/index";
-//import data from './data/data'
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // const { products } = data
+  useEffect(() => {
+    document.title='Shopping cart - Women clothes'
+  },[])
 
   return (
     <div className="App">
